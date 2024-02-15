53ce3e93e513163688cd77f5d6051a523260d409
Falconoff <sokolovserega1982@gmail.com>
2024-02-15 22:29:43 +0200

    feat: add Nav menu for mobile

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
+// ==== for Modal ===
+// const refs = {
+//   openModalBtnRef: document.querySelector("[data-open-modal]"),
+//   closeModalBtnRef: document.querySelector("[data-close-modal]"),
+//   backdropRef: document.querySelector("[data-backdrop]"),
+// };
+
+// refs.openModalBtnRef.addEventListener("click", toggleModal);
+// refs.closeModalBtnRef.addEventListener("click", toggleModal);
+
+// function toggleModal() {
+//   refs.backdropRef.classList.toggle("is-hidden");
+// }
+
+// ==== for Mobile Navigation in Header ===
 const refs = {
-  openModalBtnRef: document.querySelector("[data-open-modal]"),
-  closeModalBtnRef: document.querySelector("[data-close-modal]"),
-  backdropRef: document.querySelector("[data-backdrop]"),
+  mobileNavBtnRef: document.querySelector("[data-mobile-nav-btn]"),
+  mobileNavRef: document.querySelector("[data-mobile-nav]"),
+  navIconOpenRef: document.querySelector("[data-nav-icon-open]"),
+  navIconCloseRef: document.querySelector("[data-nav-icon-close]"),
 };
 
-refs.openModalBtnRef.addEventListener("click", toggleModal);
-refs.closeModalBtnRef.addEventListener("click", toggleModal);
+refs.mobileNavBtnRef.addEventListener("click", toggleNav);
 
-function toggleModal() {
-  refs.backdropRef.classList.toggle("is-hidden");
+function toggleNav() {
+  refs.navIconOpenRef.classList.toggle("btn-burger__icon--is-hidden");
+  refs.navIconCloseRef.classList.toggle("btn-burger__icon--is-hidden");
+  refs.mobileNavRef.classList.toggle("mobile-nav--is-hidden");
 }
